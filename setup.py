62006aa38c9a653b8e95c0502576580aa0d09cf3
Brennan Ashton <bashton@brennanashton.com>
2018-10-17 13:56:45 -0700

    Update project URL

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
     name='Flask-InfluxDB',
     version='0.2',
-    url='http://github.com/ombitron/flask-influxdb',
+    url='http://github.com/btashton/flask-influxdb',
     license='BSD',
     author='Brennan Ashton',
     author_email='brennan@ombitron.com',

a22bdc548940218c408069218c4351941c68d296
Brennan Ashton <bashton@brennanashton.com>
2020-01-05 23:38:39 -0800

    Fix packaging issue in 0.3 release
    Signed-off-by: Brennan Ashton <bashton@brennanashton.com>

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup
 
 setup(
     name="Flask-InfluxDB",
-    version="0.3",
+    version="0.3.1",
     url="http://github.com/btashton/flask-influxdb",
     license="BSD",
     author="Brennan Ashton",
     author_email="brennan@ombitron.com",
     description="Flask bindings for the InfluxDB time series database",
     long_description=__doc__,
-    py_modules=["flask_influxdb"],
+    packages=["flask_influxdb"],
     zip_safe=False,
     include_package_data=True,
     platforms="any",
